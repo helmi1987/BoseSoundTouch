13ce89f0d2f196b93f16f75ecd7f9af4e3ee0568
helmi1987 <diethelm.stefan@gmail.com>
2016-07-04 22:36:49 +0200

    Revert "SoundTouch"
    This reverts commit 3da829bc9178ce7da9150f6edaa70fc3316dfc35.

--- SoundTouch/BoseWakeUp.ps1
+++
@@ -1,7 +0,0 @@
-﻿Import-Module .\BoseSoundTouch.psm1
-
-#Generate Random ;)
-$Preset = Get-Random -Minimum 1 -Maximum 6
-
-#Run SoundTouch with Random Preset :)
-BoseSoundTouch -SoundTouchIP <IP or DNS> -SetVolume 50 -SetPreset $Preset
