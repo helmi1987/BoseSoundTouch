e45b589dab8481a028a8c83bbf2bae2a2219def2
helmi1987 <diethelm.stefan@gmail.com>
2016-11-17 00:49:42 +0100

    Make a Zone
    is working but not nice! I will change this after testing.

--- BoseSoundTouch/BoseWakeUp.ps1
+++ BoseSoundTouch/BoseWakeUp.ps1
@@ -3,5 +3,18 @@
 #Generate Random ;)
 $Preset = Get-Random -Minimum 1 -Maximum 6
 
-#Run SoundTouch with Random Preset :)
-BoseSoundTouch -SoundTouchIP <IP or DNS> -SetVolume 50 -SetPreset $Preset
+$Master = "192.168.1.67"
+$Slave  = "192.168.1.102"
+
+#Set SoundTouch to low Volume Level (Power on qual volume = 10)
+BoseSoundTouch -SoundTouchIP $Master -SetVolume 1 -Power off
+BoseSoundTouch -SoundTouchIP $Slave  -SetVolume 1 -Power off
+
+#Make Group (testing)
+BoseSoundTouch -SoundTouchIP $Master -Power on -PostApiKey "setZone" -ApiXml "<zone master='8030DC6C68BE'><member ipaddress='$Master' role='NORMAL'>8030DC6C68BE</member><member ipaddress='$Slave ' role='NORMAL'>B0D5CCBE08EB</member></zone>"
+
+#Set Preset and Volume
+BoseSoundTouch -SoundTouchIP $Master -SetVolume 50 -SetPreset $Preset -VolumeFadeTime 60 -SendKey REPEAT_ALL
+
+#Set Volume second speaker
+BoseSoundTouch -SoundTouchIP $Slave  -SetVolume 50 -VolumeFadeTime 20
